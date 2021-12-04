481f86f316c484d4b687a754efbc2ddc9b7c69a0
Sergey Heinrich <hans.nordwand@gmail.com>
2021-12-04 23:10:40 +0300

    Add states into Tasks

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     description
     author factory: :manager
     assignee factory: :developer
-    state { [:new_task, :in_development, :archived, :in_qa, :in_code_review, :ready_for_release, :released].sample.to_s }
     expired_at { Time.now + 24.hours }
   end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -5,4 +5,15 @@ class TaskTest < ActiveSupport::TestCase
     task = create(:task)
     assert task.persisted?
   end
+
+  test 'the validity of transition StateMachine states of Tasks' do
+    task = create(:task)
+    assert task.new_task?
+
+    { :into_development => :in_development, :into_qa => :in_qa, :into_code_review => :in_code_review,
+      :into_ready_for_release => :ready_for_release, :into_released => :released, :into_archived => :archived }.each_pair do |event, state|
+      eval("task.#{event}")
+      assert task.state?(state)
+    end
+  end
 end
